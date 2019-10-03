a36a28fd180d12fb6c7699ae87bf190368926f75
Janay Bailey <tjanay87@gmail.com>
2019-10-03 21:38:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,8 +42,7 @@ class Song
     split_file = song.split(" - ")
     artist = split_file[0]
     song_name = split_file[1].split(".")[0]
-    song = Song.new
-    song.name = song_name
+    song = self.new_by_name(song_name)
     song.artist_name = artist
     song
   end
