ab9953d387ab6a7a1fb2a197045445bd7b3518bc
Janay Bailey <tjanay87@gmail.com>
2019-10-03 20:31:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,9 +5,21 @@ class Song
   def self.all
     @@all
   end
-
+  
   def save
     self.class.all << self
+  end
+
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.create
+    song.name = song_name 
+    song
   end
 
 end
